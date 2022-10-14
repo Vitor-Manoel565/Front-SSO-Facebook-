e62a7ebb3d632b064fdf1dc77c94ba52b9c7d803
Vitor-Manoel565 <vitoralvesbvgs@gmail.com>
2022-10-14 16:53:17 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  // const user = false;
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getUsers = ()=>fetch("/auth/user", {
+      method: "GET, POST, PUT, DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "acess-control-allow-origin": true,
+      },
+      credentials: "include",
+    }).then((response)=>response.json()).then((data)=>setUser(data));
+    getUsers();
+  }, []);
+  const Facebook = () => {
+    window.open("http://localhost:5000/auth/facebook", "_self");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <h1>Forms</h1>
+        <input></input>
+        <input></input>
+        <button>LOGAR</button>
+        <hr className="line"></hr>
+        <div className="containerButton">
+          <button className="facebook" onClick={Facebook}>Facebook</button>
+          <button className="facebook">Google</button>
+
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/formLogin/index.js
@@ -0,0 +1,12 @@
+import * as S from "./style";
+
+export default function FormLogin() {
+  return (
+    <S.containerForm>
+      <S.buttonLogin>
+        Login
+        <S.logoFacebook src="/facebook.png" />
+      </S.buttonLogin>
+    </S.containerForm>
+  );
+}
