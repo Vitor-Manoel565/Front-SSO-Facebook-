ea09748b64bdd90d5f975183a007807f7c6f696b
Vitor-Manoel565 <vitoralvesbvgs@gmail.com>
2022-10-20 12:42:35 -0300

    add new

--- src/App.js
+++ src/App.js
@@ -37,15 +37,17 @@ const FacebookLoginPage = () => {
       statusChangeCallback(response);
     });
   };
-  const statusChangeCallback = (response) => {
-    if (response.status === "connected") {
-      testAPI();
-      console.log(response);
-    } else {
-      document.getElementById("status").innerHTML =
-        "Please log " + "into this app.";
+  function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+    console.log('statusChangeCallback');
+    console.log(response);                   // The current login status of the person.
+    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+      testAPI();  
+    } else {                                 // Not logged into your webpage or we are unable to tell.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this webpage.';
     }
-  };
+  }
+
   
   // Check login state
 
