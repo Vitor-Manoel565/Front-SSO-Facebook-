20783afbf6ba2bd77555159624caf086884209f0
Vitor-Manoel565 <vitoralvesbvgs@gmail.com>
2022-10-20 15:32:28 -0300

    add new bibli

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
+import FacebookLogin from "react-facebook-login";
 
-import FacebookLogin from 'react-facebook-login';
+const App = () => {
+  const responseFacebook = (response) => {
+    console.log(response);
+  };
 
- const App = () => {
-
-const responseFacebook = (response) => {
-  console.log(response);
-}
-
-return (
-  <FacebookLogin
-    appId="1088597931155576"
-    autoLoad={true}
-    fields="name,email,picture"
-    onClick={console.log("clicado!")}
-    callback={responseFacebook} />
-);
-
-}
-
+  return (
+    <FacebookLogin
+      appId="1147465239309646"
+      autoLoad={true}
+      fields="name,email,picture"
+      onClick={console.log("clicado!")}
+      callback={responseFacebook}
+    />
+  );
+};
 
 export default App;
