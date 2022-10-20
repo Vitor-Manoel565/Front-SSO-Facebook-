07ef8a8bd9ded917da4364665e17d4c37d4197f7
Vitor-Manoel565 <vitoralvesbvgs@gmail.com>
2022-10-20 13:12:32 -0300

    add new

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const FacebookLoginPage = () => {
   // Login with Facebook with react
+  const [initialized, setInitialized] = useState(false);
   useEffect(() => {
     // Load the SDK asynchronously
-    (function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {
-        return;
-      }
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
+   
   }, []);
 
   // Initialize and add the facebook script
@@ -29,24 +20,40 @@ const FacebookLoginPage = () => {
       });
 
       window.FB.AppEvents.logPageView();
+      console.log("FB initialized");
+      setInitialized(true);
     };
   }, []);
 
+
+
+  const statusChangeCallback = (response) => {
+    console.log("statusChangeCallback", response);
+    if (response.status === "connected") {
+      console.log("Logged in and authenticated");
+      testAPI();
+    } else {
+      console.log("Not authenticated");
+    }
+
+  };
   const checkLoginState = () => {
     window.FB.getLoginStatus(function (response) {
       statusChangeCallback(response);
     });
   };
-  function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
-    console.log('statusChangeCallback');
-    console.log(response);                   // The current login status of the person.
-    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
-      testAPI();  
-    } else {                                 // Not logged into your webpage or we are unable to tell.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this webpage.';
-    }
-  }
+
+  const testAPI = () => {
+    console.log("Welcome!  Fetching your information.... ");
+    window.FB.api("/me", function (response) {
+      console.log("Successful login for: " + response.name);
+      document.getElementById("status").innerHTML = `Thanks for logging in, ${
+        response.name
+      }!`;
+    });
+  };
+  
+
 
   
   // Check login state
@@ -54,14 +61,6 @@ const FacebookLoginPage = () => {
   // Handle the response
 
   // Test API
-  const testAPI = () => {
-    console.log("Welcome!  Fetching your information.... ");
-    window.FB.api("/me", function (response) {
-      console.log("Successful login for: " + response.name);
-      document.getElementById("status").innerHTML =
-        "Thanks for logging in, " + response.name + "!";
-    });
-  };
 
   
 
