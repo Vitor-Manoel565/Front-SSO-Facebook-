ebddebd005a7aa04b7faf65dfd76000cb8a4c16c
Vitor-Manoel565 <vitoralvesbvgs@gmail.com>
2022-10-25 15:15:49 -0300

    add instagram login

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import FacebookLogin from "react-facebook-login";
+// import FacebookLogin from "react-facebook-login";
+import  InstagramLogin  from "react-instagram-login";
 
 const App = () => {
-  const responseFacebook = (response) => {
+  const responseInstagram = (response) => {
     console.log(response);
   };
 
   return (
-    <FacebookLogin
-      appId="1147465239309646"
-      autoLoad={true}
-      fields="name,email,picture"
-      onClick={()=> console.log("clicado!")}
-      callback={responseFacebook}
+    <InstagramLogin
+      clientId="572050501359312"
+      buttonText="Login"
+      onSuccess={responseInstagram}
+      onFailure={responseInstagram}
     />
   );
 };
