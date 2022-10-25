62b4068a85d8e60c42d7104469cf1af10239e9d8
Vitor-Manoel565 <vitoralvesbvgs@gmail.com>
2022-10-25 16:43:43 -0300

    add new modified

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 // import FacebookLogin from "react-facebook-login";
-import  InstagramLogin  from "react-instagram-login";
+import InstagramLogin from "react-instagram-login";
 import { useState } from "react";
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [token, setToken] = useState("");
+  const [token, setToken] = useState({});
   const responseInstagram = (response) => {
-    if(!response.error_type){
+    if (!response.error_type) {
       setIsLoggedIn(true);
       const accessToken = response;
-      setToken(accessToken)
+      setToken(accessToken);
       console.log("Você está logado, seu token é: ", accessToken);
-
     }
   };
 
   return (
-    <InstagramLogin
-      clientId="1296989504384057"
-      buttonText="Login"
-      onSuccess={responseInstagram}
-      onFailure={responseInstagram}
-      scope="user_profile,user_media"
-    />
+    <div className="App">
+      {isLoggedIn ? (
+        <div>
+          <h1>Olá, você está logado!</h1>
+          <h2>Seu token é: {token}</h2>
+        </div>
+      ) : (
+        <InstagramLogin
+          clientId="1296989504384057"
+          buttonText="Login"
+          onSuccess={responseInstagram}
+          onFailure={responseInstagram}
+          scope="user_profile,user_media"
+        />
+      )}
+    </div>
   );
 };
 
